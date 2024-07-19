d4b5795ed6428dcc2e724903837b119181edbf1b
MAX-G <go1991woo@naver.com>
2024-07-19 17:01:31 +0900

    240719 수업자료 커밋

---
+++ 05_oop/src/cls/pkg1/ClassRun1.java
@@ -0,0 +1,23 @@
+package cls.pkg1;
+
+public class ClassRun1 {
+
+	public static void main(String[] args) {
+
+		// public class + 같은 패키지
+		// import 하여 객체 생성하기 -> 문제 o/x
+		ClassTest1 t1 = new ClassTest1();
+		t1.method();
+		
+		
+		// (default) class + 같은 패키지
+		// DefaultClass 객체 생성 -> 문제 X
+		DefaultClass d1 = new DefaultClass();
+		d1.method();
+		
+		
+		
+		
+	}
+
+}
---
+++ 05_oop/src/cls/pkg1/DefaultClass.java
@@ -0,0 +1,9 @@
+package cls.pkg1;
+
+// (Default) 클래스 : 같은 패키지만 접근 가능
+class DefaultClass {
+
+	public void method() {
+		System.out.println("(Default)는 같은 패키지만 접근 가능");
+	}
+}
